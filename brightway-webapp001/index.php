1f54e99291d0ace6684cc31453d48f32ec24716a
Samuelale1 <alesamuelayomide@gmail.com>
2025-07-09 09:49:03 +0100

    i added a landing page favicon and logo

---
+++ brightway-webapp001/index.php
@@ -0,0 +1,17 @@
+<!-- Landing page of Brightway webapp-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Brightway Landing page</title>
+ <link rel="icon" type="image/x-icon" href="brightway-webapp001\images\Brightway-logo.png">
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+
